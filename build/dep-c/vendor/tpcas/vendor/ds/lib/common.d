a1f8014aa9c865906a9ce8c73105a562abad6a74
hydrastro <9256943+hydrastro@users.noreply.github.com>
2026-06-01 09:26:37 +0200

    dist clean

--- build/dep-c/vendor/tpcas/vendor/ds/lib/common.d
+++
@@ -1,4 +0,0 @@
-build/obj-c/vendor/tpcas/vendor/ds/lib/common.o: \
-  vendor/tpcas/vendor/ds/lib/common.c \
-  vendor/tpcas/vendor/ds/lib/common.h
-vendor/tpcas/vendor/ds/lib/common.h:
--- build/dep-c/vendor/tpcas/vendor/ds/lib/status.d
+++
@@ -1,4 +0,0 @@
-build/obj-c/vendor/tpcas/vendor/ds/lib/status.o: \
-  vendor/tpcas/vendor/ds/lib/status.c \
-  vendor/tpcas/vendor/ds/lib/status.h
-vendor/tpcas/vendor/ds/lib/status.h:
